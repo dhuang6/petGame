92246982c870d3d4e409388b867eafa41f953906
Daniel Huang <dhuang684@gmail.com>
2019-03-12 20:45:53 -0400

    Add files via upload
    created random generators for pet choices.
    included attributes and properties.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+//setup default webpage
+function init(){
+  var start;
+  
+  
+}
+
+
+
 var pets = {
+      
+    
     lizard: {
       name: 'lizard',
       personality: 'friendly',
@@ -6,7 +17,7 @@ var pets = {
       greet: function(){
         alert('hiss');
       },
-      hunger: 5, //not hungry
+      hunger: 6, //not hungry
       
     },
     slug: {
@@ -16,7 +27,7 @@ var pets = {
       greet: function(){
         alert('blinks eyes');
       },
-      hunger: 5, //not hungry
+      hunger: 6, //not hungry
       
     },
     rabbit: {
@@ -26,7 +37,7 @@ var pets = {
       greet: function(){
         alert('hop, hop');
       },
-      hunger: 4, //not hungry
+      hunger: 4, //a bit hungry
       
     },
     cat : {
@@ -49,15 +60,7 @@ var pets = {
       },
       hunger: 3,//should eat soon.
     },
-    parrot: {
-      name: 'parrot',
-      personality: 'friendly',
-      energy: 'high',
-      greet : function(){
-        alert('SQUAKK');
-      },
-      hunger: 3,//should eat soon.
-    },
+   
     duck: {
       name: 'duck',
       personality: 'friendly',
@@ -88,7 +91,6 @@ var pets = {
       
     },
    owl : {
-      code: 3,
       name: 'owl',
       personality: 'friendly',
       energy: 'low',
@@ -130,6 +132,7 @@ var pets = {
   
 }
 
+
 function genPetTier(){
   var tier;
   //gen a random num btwn 1 and 4
@@ -148,41 +151,83 @@ function genPetCode(){
 petCodes = {// 4 levels with 3 pets at each level.
   code1 : ['lizard','slug','rabbit'],
   code2: ['cat','dog','duck'],
-  code3: ['cow', 'seal','owl'],
-  code4: ['monkey','trex','unicorn'],
+  code3: ['cow', 'seal','owl' ],
+  code4: ['monkey','tRex','unicorn'],
 }
-
-function getPet(){
-  var pet,num,secondNum,chosenNum, count;
- 
+  
+function getPet(){//eventually want to add in logic that only allows you to select 3x.
+  var pet,petInfo,num,secondNum,chosenNum, count, petImage;
+  
+  petInfo = document.getElementById('displayArea');
   num = genPetTier();
   chosenNum = num;
     console.log(chosenNum);
   
   if(chosenNum === 1){
     secondNum = genPetCode();
+    //selects a random pet from the specified tier.
       pet = petCodes.code1[secondNum];
-      count++;
       console.log(pet);
+      petInfo.textContent = pets[pet].name;
+      pets[pet].greet();
+      displayPet(pet);
+      
+      return pet;
       
   }
   
   else if(chosenNum === 2){
     secondNum = genPetCode();
       pet = petCodes.code2[secondNum];
-      count++;
-      console.log(pet);
+      petInfo.textContent = pets[pet].name;
+      pets[pet].greet();
+      displayPet(pet);
+      
+  
+      return pet;
   }
   else if(chosenNum === 3){
     secondNum = genPetCode();
       pet = petCodes.code3[secondNum];
-      count++;
-      console.log(pet);
+      petInfo.textContent = pets[pet].name;
+      pets[pet].greet();
+      displayPet(pet);
+      
+      return pet;
   }
   else if(chosenNum === 4){
     secondNum = genPetCode();
       pet = petCodes.code4[secondNum];
-      count++;
-      console.log(pet)
+      petInfo.textContent = pets[pet].name;
+      pets[pet].greet();
+      displayPet(pet);
+      return pet;
     }
 }
+
+//after end user has chosen pet
+function displayPet(pet){
+  var chosenPet,description;
+  description = document.getElementById('displayArea');
+  //this.pet = chosenPet;
+  //chosenPet = pets[pet];
+  
+  if(pets[pet].hunger > 5){
+     description.textContent =`Your pet is a ${pets[pet].name} it's temperment is ${pets[pet].personality} and is currently not hungry`;
+    //console.log(`your pet ${pets[pet].name} is not hungry `);
+   
+  } else if(pets[pet].hunger < 5 || pets[pet].hunger > 2 ){
+    description.textContent =`Your pet is a ${pets[pet].name} it's temperment is ${pets[pet].personality} and is currently hungry`;
+    //console.log(`your pet ${pets[pet].name} is hungry `);
+    
+  } else {
+    //console.log(`your pet ${pets[pet].name} is about to leave to live with their counsin.`);
+   description.textContent =`Your pet is a ${pets[pet].name} it's temperment is ${pets[pet].personality} and is currently about to leave to live with their counsin.`;
+  }
+  
+  //description.textContent =`Your pet is a ${pets[pet].name} it's temperment is ${pets[pet].personality} and is currently ${pets[pet].hunger}`;
+
+  
+}
+
+//play games with pet.
