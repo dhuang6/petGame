ede912e8c8332cfa6ceca9987908f8f513fbdf35
Daniel Huang <dhuang684@gmail.com>
2019-03-14 13:52:54 -0400

    Add files via upload
    updated button for confirming pet.
    updated hide and show buttons to display buttons as needed.

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
 //global variables
-var hungerlvl, petCodes,count, petSelected;
-//allow end user to select a new pet 3x.
+var hungerlvl, petCodes,count, petSelected,userConfirmed, food;
+//allow end user to select a new pet 3x. react.js
 
+//look into callbacks of functions for some of their data.
 //setup default webpage
 function init(){
   var start;
+   
   start = true;
   if(start){
-count = 0;
-petSelected = false;
+    //num of pet rolls done by end user.
+    count = 0;
+    petSelected = false;
+    //end user has no food on hand at the beginning.
+    food = 0;
+    userConfirmed = document.getElementById('confirmPet');
   }
- //implement luck.
 }
 
 init();
 
 
+//make a game over pet leaves function.
 
-function selectPetCount(){
-  var btnClicked;
-  //count isn't increasing past 1 which means each new pet selection is separated.
-  count = 0;
-  btnClicked = document.querySelector('#petSelect');
-  console.log(btnClicked);
-  if(genPetCode){
-    count ++;
-  console.log(count);
-    if(count > 3){
-      console.log(count);
-    return alert('sorry you have selected a pet 3x and cannot choose again!');
-  }
-    
-     
-  }
-  
- 
-}
 
+//move to another sheet // or use a framework //
 var pets = {
       //common//
     lizard: {
@@ -164,7 +152,6 @@ var pets = {
 function genPetTier(){
   var tier;
   //gen a random num btwn 1 and 4 to select a tier.
-  //Math.floor(Math.random() *4) + 1;
   tier = Math.floor(Math.random() *4) + 1;
     return tier;
 }
@@ -172,14 +159,14 @@ function genPetTier(){
 function genPetCode(){
   var code;
   
-  //gen a random num btwn 0 and 2 b/c there are 3 animals
+  //gen a random num btwn 0 and 2 b/c there are 3 animals a tier.
   code = Math.floor(Math.random() *3);
   
     return code;
 }
 
 petCodes = {// 4 levels with 3 pets at each level.
-  code1 : ['lizard','slug','rabbit'],
+  code1: ['lizard','slug','rabbit'],
   code2: ['cat','dog','duck'],
   code3: ['cow', 'seal','owl' ],
   code4: ['monkey','tRex','unicorn'],
@@ -193,12 +180,21 @@ function isPetHungry(){
   } else if(hungerlvl > 4 && hungerlvl < 2){
     return 1;
   } else {//I want to make it so that the pet is thinking of leaving at this point.
+  //need a new function  petWantsOut()
     return 2;
   }
 }
+
+  var feed;
+  if(food >0){
+    feed = document.getElementById('feedPet').addEventListener('click', function(e){
+      console.log(e);
+    })
+  }
+
   
-function getPet(){//eventually want to add in logic that only allows you to select 3x.
-  var pet,petInfo,num,secondNum,chosenNum, petImage;
+function getPet(){
+  var pet,petInfo,secondNum,chosenNum, petImage;
   //show the gen info for new pet.
   petInfo = document.getElementById('displayArea');
   
@@ -221,9 +217,7 @@ function getPet(){//eventually want to add in logic that only allows you to sele
       //displays the pet picture based on petCodes.
       petImage.src='default' + pets[pet].name +'.jpeg';
       
-      
-      
-  }
+    }
   
   else if(chosenNum === 2){
     secondNum = genPetCode();
@@ -231,7 +225,7 @@ function getPet(){//eventually want to add in logic that only allows you to sele
       petInfo.textContent = pets[pet].name;
       pets[pet].greet();
       displayPet(pet);
-      petImage.src='default' + pets[pet].name+'.jpeg';
+      petImage.src='default' + pets[pet].name +'.jpeg';
   
       
   }
@@ -241,8 +235,7 @@ function getPet(){//eventually want to add in logic that only allows you to sele
       petInfo.textContent = pets[pet].name;
       pets[pet].greet();
       displayPet(pet);
-      petImage.src='default' + pets[pet].name+'.jpeg';
-    
+      petImage.src='default' + pets[pet].name +'.jpeg';
       
   }
   else if(chosenNum === 4){
@@ -251,16 +244,18 @@ function getPet(){//eventually want to add in logic that only allows you to sele
       petInfo.textContent = pets[pet].name;
       pets[pet].greet();
       displayPet(pet);
-      petImage.src='default' + pets[pet].name+'.jpeg';
+      petImage.src='default' + pets[pet].name +'.jpeg';
       
       }
+      //increases per pick for the 3 picks.
       count++;
       console.log(count);
-      return pet, chosenNum;
+      return pet;
     }
     
     else if(petSelected === true){
       alert('congrats on choosing your new pet!');
+      hideBtns();
     }
     else {
       alert('sorry you have rolled for a pet 3x!');
@@ -271,40 +266,39 @@ function showBtns(){
   //displays buttons related to pet after pet has been chosen.
   var showFeedBtn, showPlayBtn;
   
-  if(petSelected || count ===3){
+  if(petSelected === true || count > 0){
     showPlayBtn = document.getElementById('playGames').style.visibility ='visible';
-    showFeedBtn = document.getElementById('feed').style.visibility ='visible';
-   // return showFeedBtn, showPlayBtn;
+    showFeedBtn = document.getElementById('feedPet').style.visibility ='visible';
   }
 }
 
 function hideBtns(){
-  var hideChoosePetBtn, petChosen, petChosenVal;
+  var hideChoosePetBtn, hideConfirmPetBtn;
   //hide btns that aren't needed after pet has been chosen.
-  if(petSelected || count > 3 ){
-  if(genPetCode){
+  if(petSelected === true || count > 3 ){
+    hideConfirmPetBtn = document.getElementById('confirmPet').style.visibility = 'hidden';
     hideChoosePetBtn = document.getElementById('start').style.visibility = 'hidden';
-    return hideChoosePetBtn;
-    }
   }
 }
 
-//button for making it so you can't change pets.
-function petConfirmed(){
-  var userConfirmed;
-  userConfirmed = document.getElementById('confirmPet').addEventListener('click', function(e){
-        userConfirmed = true;
+//button for confirming new pet selection.
+userConfirmed.addEventListener('click', function(e){
+    if(count > 0 ){//can only click this if 1 selection has been made.
+       userConfirmed = true;
        petSelected = true;
        console.log(userConfirmed);
       alert('congrats on your new pet!');
+      //hide the roll new pet button and confirm pet.
+      hideBtns();
+      
+      //show the feed pet and games button.
+      showBtns();
       //petSelected
       return petSelected;
-
-  });
- 
-}
-
-
+    }
+});
+  
+  
 //after end user has chosen pet
 function displayPet(pet){
   //need to add in images for each pet next.
@@ -313,22 +307,25 @@ function displayPet(pet){
   //this.pet = chosenPet;
   //chosenPet = pets[pet];
   console.log(isPetHungry(pet));
-  showBtns();
+
   
   if(isPetHungry(pet) > 5){
      description.textContent =`Your pet is a ${pets[pet].name} it's temperment is ${pets[pet].personality} and is currently not hungry`;
+     
+    
      
     //console.log(`your pet ${pets[pet].name} is not hungry `);
    
   } else if(isPetHungry(pet) < 5 || isPetHungry(pet) > 2 ){
     description.textContent =`Your pet is a ${pets[pet].name} it's temperment is ${pets[pet].personality} and is currently hungry`;
+      
   
     //console.log(`your pet ${pets[pet].name} is hungry `);
     
   } else {
     //console.log(`your pet ${pets[pet].name} is about to leave to live with their counsin.`);
    description.textContent =`Your pet is a ${pets[pet].name} it's temperment is ${pets[pet].personality} and is currently about to leave to live with their counsin.`;
-  
+       
   }
   
   //description.textContent =`Your pet is a ${pets[pet].name} it's temperment is ${pets[pet].personality} and is currently ${pets[pet].hunger}`;
